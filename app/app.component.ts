846af1c0be92d2e2f4cd46f65d7f995563274c6d
Ivan Markovic <ivan.markovic.2464@metropolitan.ac.rs>
2016-08-20 16:44:49 +0200

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,26 @@
+import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+
+import {RegistracijaComponent} from 'app/registracija/registracija.component';
+import {LoginComponent} from 'app/login/login.component';
+import {MainPageComponent} from 'app/mainpage/mainpage.component';
+import {Sobe} from 'app/sobe/sobe.component';
+import {Dodaj} from 'app/dodaj/dodaj.component';
+import {Pipe} from 'angular2/core';
+
+@Component({
+	selector: 'moja-aplikacija',
+	templateUrl: 'app/router.html',
+	directives: [ROUTER_DIRECTIVES]
+	})
+
+@RouteConfig([
+  {path:'/', name: 'MainPage', component: MainPageComponent, useAsDefault: true},
+  {path:'/registracija', name:'Registracija', component: RegistracijaComponent},
+  {path:'/login', name:'Login', component: LoginComponent},
+  {path:'/sobe', name: 'Sobe', component: Sobe},
+  {path:'/dodaj', name: 'Dodaj', component: Dodaj}
+  
+])
+
+export class AppComponent { }
